a901408abf19408d6ecaa3f55b9ec01ef0232752
Germanno Teles <germannoteles@gmail.com>
2024-09-01 18:17:20 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/diferenciado/agendaaibackend/AgendaAiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.diferenciado.agendaaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaAiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/diferenciado/agendaaibackend/AgendaAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diferenciado.agendaaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
