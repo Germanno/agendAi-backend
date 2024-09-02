37efebc7ccb8848a1c85617072fc59c9f5369e0a
Germanno Teles <germannoteles@gmail.com>
2024-09-01 22:49:50 -0300

    Criado o ErrorMessageDTO para armazenar os erros, to colocando ExceptionHandler com o validation

--- src/main/java/br/com/diferenciado/agendaaibackend/modules/aluno/AlunoEntity.java
+++ src/main/java/br/com/diferenciado/agendaaibackend/modules/aluno/AlunoEntity.java
@@ -1,7 +1,10 @@
 package br.com.diferenciado.agendaaibackend.modules.aluno;
 
 import br.com.diferenciado.agendaaibackend.modules.endereco.EnderecoEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,10 +12,16 @@ import java.util.UUID;
 public class AlunoEntity {
 
     private UUID  id;
+    @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaço")
+    private String username;
     private String nome;
     private String cpf;
     private String telefone;
+
+    @Email(message = "O campo [email] deve conter um email valido   ")
     private String email;
+
+    @Length(min = 8, max = 16,message = "A senha deve ter entre (8) e (16) caracteres")
     private String password;
     private EnderecoEntity endereco;
 }
--- src/main/java/br/com/diferenciado/agendaaibackend/modules/aluno/controllers/AlunoController.java
+++ src/main/java/br/com/diferenciado/agendaaibackend/modules/aluno/controllers/AlunoController.java
@@ -1,6 +1,7 @@
 package br.com.diferenciado.agendaaibackend.modules.aluno.controllers;
 
 import br.com.diferenciado.agendaaibackend.modules.aluno.AlunoEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ public class AlunoController {
 
 
     @PostMapping("/")
-    public  void create(@RequestBody AlunoEntity alunoEntity) {
+    public  void create(@Valid @RequestBody AlunoEntity alunoEntity) {
         System.out.println("Aluno");
         System.out.println(alunoEntity.getEmail());
     }
